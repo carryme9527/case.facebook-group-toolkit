63c907f279b0f8ad7fe2174ff83772a067cd0d60
carryme9527 <carryme9527@users.noreply.github.com>
2017-04-02 01:34:33 +0800

    scripts to get comments in last 24 hours

---
+++ scripts_list_comments_in_24h.py
@@ -0,0 +1,26 @@
+import re
+import json
+import utils
+import settings
+import requests
+import facebook_util
+
+access_token = utils.get_token()
+
+results = []
+for pid in open('post_ids'):
+    pid = pid.strip()
+    path = '/{0}'.format(pid)
+    resp = facebook_util.get_comments_order_by_time(path, access_token)
+    if 'comments' not in resp.keys():
+        continue
+    for comment in resp['comments']['data'][:2]:
+        if utils.is_owner(comment):
+            continue
+        if not utils.is_in_24_hours(comment['created_time']):
+            continue
+        results.append((pid, comment['id'], uid, comment['created_time']))
+
+for r in results:
+    pid, cid, uid, ct = r
+    print pid, cid, uid, ct
