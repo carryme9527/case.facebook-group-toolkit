8c96bcac328b110d93d78a49492a985a61c8d5c4
carryme9527 <carryme9527@users.noreply.github.com>
2017-04-03 13:12:20 +0800

    change url domain for view

--- facebook_util.py
+++ facebook_util.py
@@ -1,20 +1,20 @@
 import requests
 import json
-from settings import facebook_domain, facebook_api_version
+from settings import facebook_domain, facebook_view, facebook_api_version
 
 def api_url(path):
   return facebook_domain + facebook_api_version + path
 
 def group_post_url(gid, pid):
     return '{0}/groups/{1}/permalink/{2}' \
-        .format(facebook_domain, gid, pid)
+        .format(facebook_view, gid, pid)
 
 def group_comment_url(gid, pid, cid):
     return '{0}/?comment_id={1}' \
         .format(group_post_url(gid, pid), cid)
 
 def user_url(uid):
-    return '{0}/{1}'.format(facebook_domain, uid)
+    return '{0}/{1}'.format(facebook_view, uid)
 
 def get_comments_order_by_time(path, access_token):
   url = api_url(path)
